313bfb39eba5e1cafcb2cb49c71a91d864a19e2a
Matthew Ediger <edigermatthew@gmail.com>
2025-01-14 23:44:59 -0700

    Added form action errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
+import { version } from 'next/package.json';
+
+const isCanary = version.includes('canary');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    ppr: isCanary ? 'incremental' : undefined,
+  }
 };
 
 export default nextConfig;
